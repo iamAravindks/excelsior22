5d29bdb7fb16529b98cc12b5aa163a97488f9de4
AthulJoshi <athuljoshi7@gmail.com>
2022-04-25 09:24:04 +0530

    workshops,interactions

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,11 +5,15 @@ import { StaticImage } from "gatsby-plugin-image"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import Landing from "../components/Landing/index"
+import Workshops from "../components/Workshops"
+import Interactions from "../components/Interactions"
 
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
     <Landing/>
+    <Workshops/>
+    <Interactions/>
   </Layout>
 )
 
