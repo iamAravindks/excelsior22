28fc9ca90b1a1615e206e3b0c662740364bc0514
SREEHARI P V <sreehariv07@gmail.com>
2022-04-26 00:20:16 +0530

    added images

--- src/components/Guests/Guests.js
+++ src/components/Guests/Guests.js
@@ -8,32 +8,54 @@ const Guests = () => {
       <div className={styles.guests}>
         <div className={styles.first}>
           <div className={styles.imageComponent + " " + styles.a}>
-            <div className={styles.letter}></div>
+            <div className={styles.letter}>g</div>
+            <div className={styles.content}>
+              <img />
+            </div>
           </div>
           <div className={styles.imageComponent + " " + styles.b}>
-            <div className={styles.letter}></div>
+            <div className={styles.letter}>U</div>
+            <div className={styles.content}>
+              <img />
+            </div>
           </div>
           <div className={styles.imageComponent + " " + styles.c}>
-            <div className={styles.letter}></div>
+            <div className={styles.letter}>e</div>
+            <div className={styles.content}>
+              <img />
+            </div>
           </div>
         </div>
         <div className={styles.second}>
           <div className={styles.imageComponent + " " + styles.d}>
-            <div className={styles.letter}></div>
+            <div className={styles.letter}>s</div>
+            <div className={styles.content}>
+              <img />
+            </div>
           </div>
           <div className={styles.imageComponent + " " + styles.e}>
-            <div className={styles.letter}></div>
+            <div className={styles.letter}>t</div>
+            <div className={styles.content}>
+              <img />
+            </div>
           </div>
           <div className={styles.imageComponent + " " + styles.f}>
-            <div className={styles.letter}></div>
+            <div className={styles.letter}>s</div>
+            <div className={styles.content}>
+              <img />
+            </div>
           </div>
         </div>
         <div className={styles.third}>
           <div className={styles.imageComponent + " " + styles.g}>
-            <div className={styles.letter}></div>
+            <div className={styles.content}>
+              <img />
+            </div>
           </div>
           <div className={styles.imageComponent + " " + styles.h}>
-            <div className={styles.letter}></div>
+            <div className={styles.content}>
+              <img />
+            </div>
           </div>
         </div>
       </div>
