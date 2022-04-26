1b4fa2ff6972603f8fada274142301f7e5ee530b
SREEHARI P V <sreehariv07@gmail.com>
2022-04-27 00:26:16 +0530

    edited grabbtn

--- src/components/Grabbtn/GrabBtn.js
+++ src/components/Grabbtn/GrabBtn.js
@@ -1,8 +1,15 @@
-import React from 'react'
-import * as styles from './grabbtn.module.css'
+import React from "react"
+import * as styles from "./grabbtn.module.css"
 const GrabBtn = () => {
   return (
-    <button className={styles.grabbtn}>Grab Tickets</button>
+    <button className={styles.grabbtn}>
+      <a
+        href="https://www.yepdesk.com/open-register/62638d3f46e0fb00013706b3"
+        target="_blank"
+      >
+        Grab Tickets
+      </a>
+    </button>
   )
 }
 
