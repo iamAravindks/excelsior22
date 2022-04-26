e629b07a822042dea4f32ba6cb84cbdb0e56e492
AravindKs <aravindkadaikattu@gmail.com>
2022-04-26 10:43:07 +0530

    slider

---
+++ src/components/AlumnisTalk/AlumnisTalk.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Carousel from "./Carousel"
+import * as styles from './alumni.module.css'
+import Mcarousel from './mobile/Mcarousel'
+
+const AlumnisTalk = () => {
+  return (
+        <section>
+      <h1>Alumni's Talk</h1>
+      <Carousel />
+      <Mcarousel />
+    </section>
+    
+  )
+}
+
+export default AlumnisTalk
