9277be1eb129190911cd3cbd61a533e83224df0e
AravindKs <aravindkadaikattu@gmail.com>
2022-04-26 10:45:17 +0530

    Add files via upload

---
+++ src/components/AlumnisTalk/mobile/MSlider.js
@@ -0,0 +1,27 @@
+import React from "react"
+import * as styles from "./mobile.module.css"
+
+export const Slider = ({ slides, currentSlide }) => {
+  return (
+    <>
+      {slides.map(({ id, image }, index) => {
+        console.log({ index, currentSlide })
+        return (
+          <div
+            key={id * index + 1}
+            className={`${
+              index === currentSlide
+                ? `${styles.slide} ${styles.activeSlide} `
+                : index - currentSlide === -1 ||
+                  (index === slides.length - 1 && currentSlide === 0)
+                ? `${styles.slide} ${styles.lastSlide}`
+                : ` ${styles.slide} ${styles.nextSlide}`
+            }`}
+          >
+            <img src={image} className={styles.image} />
+          </div>
+        )
+      })}
+    </>
+  )
+}
