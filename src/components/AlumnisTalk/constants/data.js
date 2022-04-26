930ab82b91063cd1b35b2ade91d998ae42310904
AravindKs <aravindkadaikattu@gmail.com>
2022-04-26 10:44:06 +0530

    data slider

---
+++ src/components/AlumnisTalk/constants/data.js
@@ -0,0 +1,48 @@
+import ImgOne from "../../../images/people1.svg"
+import ImgTwo from "../../../images/people2.svg"
+import ImgThree from "../../../images/people3.svg"
+import ImgFour from "../../../images/people4.svg"
+
+import ImageOne from "../../../images/peopleMb1.svg"
+import ImageTwo from "../../../images/peopleMb1.svg"
+import ImageThree from "../../../images/peopleMb1.svg"
+import ImageFour from "../../../images/peopleMb1.svg"
+
+
+export const peopleDesktop = [
+  {
+    id: 1,
+    image: ImgOne,
+  },
+  {
+    id: 2,
+    image: ImgTwo,
+  },
+  {
+    id: 3,
+    image: ImgThree,
+  },
+  {
+    id: 4,
+    image: ImgFour,
+  },
+]
+export const peopleMobile = [
+  {
+    id: 1,
+    image: ImageOne,
+  },
+  {
+    id: 2,
+    image: ImageTwo,
+  },
+  {
+    id: 3,
+    image: ImageThree,
+  },
+  {
+    id: 4,
+    image: ImageFour,
+  },
+
+]
